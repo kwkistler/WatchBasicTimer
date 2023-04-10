3b138d7c8e32060ccfd4f7e192722c356304dd4f
Kraig Kistler <kraigwkistler@gmail.com>
2023-04-10 15:06:03 -0700

    Initial Commit

---
+++ WatchBasicTimer Watch App/WatchBasicTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchBasicTimerApp.swift
+//  WatchBasicTimer Watch App
+//
+//  Created by Kraig Kistler on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchBasicTimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
